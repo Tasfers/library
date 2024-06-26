54f5445027612387c6e3969dd855af0b54709d47
Towa <70810089+Towux@users.noreply.github.com>
2024-06-26 20:35:03 +0300

    Update __init__.py

--- tasfers/__init__.py
+++ tasfers/__init__.py
@@ -1,4 +1,5 @@
 from .async_funcs import write_text_to_file as async_write_text_to_file
 from .sync_funcs import write_text_to_file as sync_write_text_to_file
+from .sync_funcs import text_to_bytes
 
-__all__ = ['async_write_text_to_file', 'sync_write_text_to_file']
+__all__ = ['async_write_text_to_file', 'sync_write_text_to_file', 'text_to_bytes']
