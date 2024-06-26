7e4bf60ef556e0bb1361c6b8a25c84978f0975ea
Towa <70810089+Towux@users.noreply.github.com>
2024-06-26 20:35:18 +0300

    Update async_funcs.py

--- tasfers/async_funcs.py
+++ tasfers/async_funcs.py
@@ -10,7 +10,7 @@ async def write_text_to_file(file_path: str, text: str):
     :param text: Текст для записи
     """
     try:
-        async with aiofiles.open(file_path, mode='a') as file:
+        async with aiofiles.open(file_path, mode='a', encoding='utf-8') as file:
             await file.write(text + '\n')
     except IOError as e:
         print(f"Ошибка при работе с файлом: {e}")
