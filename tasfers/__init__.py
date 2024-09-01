97fc424d4bbf2b68b8f0724dcdb3fb09c7e269a5
Towa <70810089+Towux@users.noreply.github.com>
2024-09-01 14:58:30 +0300

    Update __init__.py

--- tasfers/__init__.py
+++ tasfers/__init__.py
@@ -1,5 +1,10 @@
 from .async_funcs import write_text_to_file as async_write_text_to_file
 from .sync_funcs import write_text_to_file as sync_write_text_to_file
-from .sync_funcs import text_to_bytes
+# from .sync_funcs import text_to_bytes
+from .Encrypt import AesCBC, AsyncAesCBC
 
-__all__ = ['async_write_text_to_file', 'sync_write_text_to_file', 'text_to_bytes']
+__all__ = ['async_write_text_to_file',
+           'sync_write_text_to_file',
+           'AesCBC',
+           'AsyncAesCBC'
+           ]
